e7b3a5e9008a27de4aae19abd2c99640adf94d0f
Oleksii Aleksieiev <reneguarde@gmail.com>
2024-09-03 21:36:59 +0300

    Implement Profile component, add it to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,31 @@
+import css from './App.module.css';
+
+import { Profile } from '../Profile/Profile.jsx';
+
+const userData = {
+  username: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
 const App = () => {
-  return <></>;
+  return (
+    <div className={css.container}>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </div>
+  );
 };
 
 export default App;
