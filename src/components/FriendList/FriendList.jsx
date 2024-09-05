9c2dc22f90fa6299087b0f254c081c36ec856f9d
Oleksii Aleksieiev <reneguarde@gmail.com>
2024-09-05 20:28:15 +0300

    Complete second task

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,35 @@
+import css from './FriendList.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className={css['friend-card']}>
+      <img src={avatar} alt={name} className={css['friend-avatar']} />
+      <p className={css['friend-name']}>{name}</p>
+      <p
+        className={`${css['friend-status']} ${
+          isOnline ? css.online : css.offline
+        }`}
+      >
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
+    </div>
+  );
+};
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css['friend-list']}>
+      {friends.map(friend => (
+        <li key={friend.id} className={css['friend-list-item']}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App/App.jsx';
+
 import 'modern-normalize';
 import './index.css';
+
+import App from './components/App/App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
